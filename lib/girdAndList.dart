1e1daf6b7d517593695605b79a4562bda2b6cd18
hata <wmf8341@gmail.com>
2022-04-09 17:53:40 +0900

    stack

---
+++ lib/girdAndList.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+void main() {
+  debugPaintSizeEnabled = false; // Set to true for visual layout
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const showGrid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Center(child: showGrid ? _buildGrid() : _buildList()),
+      ),
+    );
+  }
+
+  Widget _buildGrid() => GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(30));
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count,
+      (i) => Container(
+              // child: Image.asset('images/pic$i.jpg'),
+              child: Container(
+            color: Colors.green,
+            child: const SizedBox(
+              width: 30,
+              height: 20,
+            ),
+          )));
+
+  Widget _buildList() {
+    return ListView(
+      children: [
+        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+        _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+            Icons.theaters),
+        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+        const Divider(),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+      ],
+    );
+  }
+
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        ),
+      ),
+      subtitle: Text(subtitle),
+      leading: Icon(
+        icon,
+        color: Colors.blue[500],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const showGrid = false;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,62 +17,33 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Center(child: showGrid ? _buildGrid() : _buildList()),
+        body: Center(child: _buildStack()),
       ),
     );
   }
 
-  Widget _buildGrid() => GridView.extent(
-      maxCrossAxisExtent: 150,
-      padding: const EdgeInsets.all(4),
-      mainAxisSpacing: 4,
-      crossAxisSpacing: 4,
-      children: _buildGridTileList(30));
-
-  List<Container> _buildGridTileList(int count) => List.generate(
-      count,
-      (i) => Container(
-              // child: Image.asset('images/pic$i.jpg'),
-              child: Container(
-            color: Colors.green,
-            child: const SizedBox(
-              width: 30,
-              height: 20,
-            ),
-          )));
-
-  Widget _buildList() {
-    return ListView(
+  Widget _buildStack() {
+    return Stack(
+      alignment: const Alignment(0.6, 0.6),
       children: [
-        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
-        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
-        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
-        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
-        _tile('United Artists Stonestown Twin', '501 Buckingham Way',
-            Icons.theaters),
-        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
-        const Divider(),
-        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-      ],
-    );
-  }
-
-  ListTile _tile(String title, String subtitle, IconData icon) {
-    return ListTile(
-      title: Text(
-        title,
-        style: const TextStyle(
-          fontWeight: FontWeight.w500,
-          fontSize: 20,
+        const CircleAvatar(
+          backgroundImage: AssetImage('../images/pic.jpg'),
+          radius: 100,
         ),
-      ),
-      subtitle: Text(subtitle),
-      leading: Icon(
-        icon,
-        color: Colors.blue[500],
-      ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.black45,
+          ),
+          child: const Text(
+            'Mia B',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
