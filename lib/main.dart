f90eb1f9e646e4cb6a08f9befbb948dd4f8b6642
hata <wmf8341@gmail.com>
2022-04-09 16:32:08 +0900

    layoutの練習

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  // debugPaintSizeEnabled = false;
+  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(const MyApp());
 }
 
@@ -13,143 +13,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter layout demo',
-      home: buildHomePage('ストロベリーパブロアのレシピ'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Center(child: _buildImageColumn()),
+      ),
     );
   }
 
-  Widget? buildHomePage(String title) {
-    const titleText = Padding(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        "ストロベリーパブロア",
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
+  // #docregion column
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26,
       ),
-    );
-
-    const subTitle = Text(
-      'パブロバは、ロシアのバレリーナにちなんで名づけられたメレンゲを使ったデザートです。'
-      'アンナ・パヴロヴァパブロバは皮がパリッとしていて、中はふんわりと軽いのが特徴です。'
-      'フルーツと生クリームをトッピングしています」。',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontFamily: 'Georgia',
-        fontSize: 25,
-      ),
-    );
-
-    var stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        const Icon(Icons.star, color: Colors.black),
-        const Icon(Icons.star, color: Colors.black),
-      ],
-    );
-
-    final ratings = Container(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          stars,
-          const Text(
-            '170 レビュー',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-
-    const descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    final iconList = DefaultTextStyle.merge(
-        style: descTextStyle,
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Icon(Icons.kitchen, color: Colors.green[500]),
-                  const Text('PREP:'),
-                  const Text('25 min'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(Icons.kitchen, color: Colors.green[500]),
-                  const Text('COOK:'),
-                  const Text('1 hr'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(Icons.kitchen, color: Colors.green[500]),
-                  const Text('FEEDS:'),
-                  const Text('4-6'),
-                ],
-              ),
-            ],
-          ),
-        ));
-
-    final leftColumn = Container(
-      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
       child: Column(
         children: [
-          titleText,
-          subTitle,
-          ratings,
-          iconList,
+          _buildImageRow(1),
+          _buildImageRow(3),
         ],
       ),
     );
-
-    final mainImage = Image.asset(
-      '../images/pavlova.jpg',
-      fit: BoxFit.cover,
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
-        height: 600,
-        child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 440,
-                child: leftColumn,
-              ),
-              mainImage,
-            ],
-          ),
-        ),
-      ),
-    );
   }
+  // #enddocregion column
+
+  // #docregion row
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.black38),
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+          ),
+          margin: const EdgeInsets.all(4),
+          child: Image.asset('images/pic$imageIndex.jpg'),
+        ),
+      );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1),
+        ],
+      );
+  // #enddocregion row
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:layouts_practice/main.dart';
+import 'package:layouts_practice/pavlova.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
